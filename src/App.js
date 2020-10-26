523bb88d23d9d94fb6847b384f31d565280e5a4f
Emmanuel Nana Antwi <sneezy300@gmail.com>
2020-10-26 17:03:46 +0000

    add App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BookApp from './components/BookApp';
+import PersonBio from './components/PersonBio';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PersonBio />
+
+      </div>
+    );
+  }
+}
+export default App;
